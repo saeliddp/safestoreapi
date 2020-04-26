31b4a24830e1044908038ec616aebe7382b30d73
Danny <saelid2517@gmail.com>
2020-04-25 22:53:31 -0700

    made lat and long longer

--- safestore/quickstart/models.py
+++ safestore/quickstart/models.py
@@ -5,8 +5,8 @@ class Store(models.Model):
     zipcode = models.CharField(max_length=20, default="00000")
     physical_address = models.CharField(max_length=100, default="NA")
     name = models.CharField(max_length=100, default="NA")
-    latitude = models.CharField(max_length=15, default="0.000000")
-    longitude = models.CharField(max_length=15, default="0.000000")
+    latitude = models.CharField(max_length=30, default="0.000000")
+    longitude = models.CharField(max_length=30, default="0.000000")
     timetable = JSONField()
     besttime = models.CharField(max_length=10, default="8:00am")
     class Meta:
