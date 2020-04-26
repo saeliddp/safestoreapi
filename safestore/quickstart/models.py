aa0d774cf70201cacd95577eb78cb916660607b2
Danny <saelid2517@gmail.com>
2020-04-25 21:40:40 -0700

    increased length of name field

--- safestore/quickstart/models.py
+++ safestore/quickstart/models.py
@@ -4,7 +4,7 @@ from jsonfield import JSONField
 class Store(models.Model):
     zipcode = models.CharField(max_length=20, default="00000")
     physical_address = models.CharField(max_length=100, default="NA")
-    name = models.CharField(max_length=30, default="NA")
+    name = models.CharField(max_length=100, default="NA")
     timetable = JSONField()
     besttime = models.CharField(max_length=10, default="8:00am")
     class Meta:
